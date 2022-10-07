495ed4fd4cae91919180c70b23f3f0914dc62ce6
Romeo Sierra <richard.powell.henshaws@gmail.com>
2022-10-07 17:43:49 +0100

    Entrypoint added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const port = process.env.PORT || 3000;
 let message = {};
 
-// app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
     res.json(message)
 });
