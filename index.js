ebae178b96fd86cc1dbd36ce6411b67b6573418c
Romeo Sierra <richard.powell.henshaws@gmail.com>
2022-10-07 18:08:14 +0100

    Entrypoint added

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-require('dotenv').config()
-const port = process.env.PORT || 3000;
+// require('dotenv').config()
+// const port = process.env.PORT || 3000;
+// let message = {};
+// // app.use(express.json)
+// // app.use(express.urlencoded({ extended: true }))
+// app.get('/', (req, res) => {
+//     res.json(message)
+// });
+
+// app.post('/', (req,res) => {
+//     message.message = req.body.msg;
+//     message.sender = req.body.sender;
+//     res.json(req.body)
+//     // res.redirect('https://romeosierra.info/ruth.html');
+// })
+
+// app.listen(port, () => {
+//     console.log(`Listening Port ${port}`)
+// });
+
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser')
+require('dotenv').config();
+const PORT = process.env.PORT;
 let message = {};
 
-app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
 app.get('/', (req, res) => {
     res.json(message)
-});
-
-app.post('/', (req,res) => {
+})
+app.post('/', (req, res) => {
     message.message = req.body.msg;
     message.sender = req.body.sender;
-    res.json(req.body.msg)
-    // res.redirect('https://romeosierra.info/ruth.html');
+    res.redirect('https://romeosierra.info/ruth.html');
 })
 
-app.listen(port, () => {
-    console.log(`Listening Port ${port}`)
-});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
